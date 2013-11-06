bd8fd0b4eea0ccdf79f187a351fc26e658c28e39
panostser1@gmail.com <panostser1@gmail.com@66860919-3fcc-04c9-45bb-785dd833bd12>
2013-11-06 19:38:21 +0000

    Ως διαχειριστή θέλω να με ενημερώνει όταν θα γεμίζουν όλες οι θέσεις.

---
+++ branches/dev-issue-4/MyNightOut/src/mynightout/exceptions/DaoException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mynightout.exceptions;
+
+/**
+ * Μια εξαίρεση που δημιουργείτε οποτεδήποτε αποτύχουμε
+ * στο να σώσουμε ή να διαβάσουμε δεδομένα από το σύστημα
+ * μόνιμης αποθήκευσης 
+ * @author Dimitris
+ */
+public class DaoException extends Exception{
+    
+}
---
+++ branches/dev-issue-4/MyNightOut/src/mynightout/model/Reservation.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mynightout.model;
+
+/**
+ * Μια απλή κλάση που αναπαριστά μια κράτηση.
+ * @author Dimitris
+ */
+public class Reservation {
+    private String customerName;
+    private int reservationId;
+    private boolean canceSuccessfulll;
+    
+    public String getCustomerName(){
+        return customerName;
+    }
+    
+    public void setCustomerName(String customerName){
+        this.customerName=customerName;
+    }
+    
+    public int getReservationId(){
+        return reservationId;
+    }
+    
+    public void setReservationId(int reservationId){
+        this.reservationId=reservationId;
+    }
+
+    public boolean isCancelSuccessfull() {
+        return canceSuccessfulll;
+    }
+
+    public void setCancelSuccessfull(boolean successCancel) {
+        this.canceSuccessfulll = successCancel;
+    }
+    
+    
+    
+}
