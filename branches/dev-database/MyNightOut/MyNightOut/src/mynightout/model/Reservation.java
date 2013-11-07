08d92cc65ad59681a1ead7a97d81a6e11c423a3e
ioanna65@hotmail.com <ioanna65@hotmail.com@66860919-3fcc-04c9-45bb-785dd833bd12>
2013-11-07 16:29:46 +0000

    new branch for the database

---
+++ branches/dev-database/MyNightOut/MyNightOut/src/mynightout/model/Reservation.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mynightout.model;
+
+/**
+ * Μια απλή κλάση που αναπαριστά μια κράτηση.
+ * @author Dimitris
+ */
+public class Reservation {
+    private String customerName;
+    private int reservationId;
+    private boolean cancelSuccessful;
+    
+    public String getCustomerName(){
+        return customerName;
+    }
+    
+    public void setCustomerName(String customerName){
+        this.customerName=customerName;
+    }
+    
+    public int getReservationId(){
+        return reservationId;
+    }
+    
+    public void setReservationId(int reservationId){
+        this.reservationId=reservationId;
+    }
+
+    public boolean isCancelSuccessful() {
+        return cancelSuccessful;
+    }
+
+    public void setCancelSuccessful(boolean cancelSuccessful) {
+        this.cancelSuccessful = cancelSuccessful;
+    }
+
+    
+    
+    
+    
+}
