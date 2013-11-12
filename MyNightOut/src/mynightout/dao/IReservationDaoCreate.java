5fee0caa9c7a779154190819124ef8b8001fa2a4
kalxas <kalxas@gmail.com>
2013-11-12 20:38:06 +0000

    issue 20 trunked!

---
+++ MyNightOut/src/mynightout/dao/IReservationDaoCreate.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mynightout.dao;
+
+import java.util.Date;
+import mynightout.exceptions.DaoException;
+import mynightout.model.Reservation;
+
+/**
+ *
+ * @author Miltos
+ */
+public interface IReservationDaoCreate {
+
+    Reservation createReservation(String customerName, Date reservationDate,
+            int numberParty, String storeName) throws DaoException;
+}
--- MyNightOut/src/mynightout/model/Reservation.java
+++ MyNightOut/src/mynightout/model/Reservation.java
@@ -3,38 +3,85 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package mynightout.model;
+
+import java.util.Date;
 
 /**
  * Μια απλή κλάση που αναπαριστά μια κράτηση.
+ *
  * @author Dimitris
  */
 public class Reservation {
+
     private String userName;
+    private String storeName;
+    private Date reservationDate;
+    private int seatNumber;
     private int reservationId;
+    private boolean successCreate;
+    private boolean cancelSuccessful;
 
     public Reservation(String customerName, int reservationId) {
         this.userName = customerName;
         this.reservationId = reservationId;
     }
-    
-    private boolean cancelSuccessful;
-    
-    public String getUserName(){
+
+    public Reservation(String customerName,
+            Date reservationDate, int seatNumber, String storeName) {
+        this.userName = customerName;
+        this.reservationDate = reservationDate;
+        this.storeName = storeName;
+        this.seatNumber = seatNumber;
+
+    }
+
+    public String getUserName() {
         return userName;
     }
-    
-    public void setUserName(String userName){
-        this.userName=userName;
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
-    
-    public int getReservationId(){
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public Date getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(Date reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public int getReservationId() {
         return reservationId;
     }
-    
-    public void setReservationId(int reservationId){
-        this.reservationId=reservationId;
+
+    public void setReservationId(int reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public boolean isSuccessCreate() {
+        return successCreate;
+    }
+
+    public void setSuccessCreate(boolean successCreate) {
+        this.successCreate = successCreate;
     }
 
     public boolean isCancelSuccessful() {
@@ -44,9 +91,4 @@ public class Reservation {
     public void setCancelSuccessful(boolean cancelSuccessful) {
         this.cancelSuccessful = cancelSuccessful;
     }
-
-    
-    
-    
-    
 }
