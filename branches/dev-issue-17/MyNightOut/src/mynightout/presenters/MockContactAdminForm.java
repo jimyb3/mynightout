5a4b39e563ef6fd057e027ec2ecab044b0ba7539
panostser1@gmail.com <panostser1@gmail.com@66860919-3fcc-04c9-45bb-785dd833bd12>
2013-11-19 01:29:44 +0000

    Δημιουργία παραθύρου βοήθειας και αντιμετώπιση προβλημάτων.

---
+++ branches/dev-issue-17/MyNightOut/src/mynightout/presenters/MockContactAdminForm.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mynightout.presenters;
+
+import mynightout.ui.IContactAdminForm;
+
+/**
+ *
+ * @author panos
+ */
+public class MockContactAdminForm implements IContactAdminForm {
+    // @Override
+   // public void display(String msg) {
+       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    //}
+
+    @Override
+    public void sendHelpDisplay(String mes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
