c3632222a2d97f494ba156d2058e86a287f686a8
punisherakias2 <punisherakias2@hotmail.com>
2013-11-20 23:48:41 +0000

    Ως διαχειριστής του προγράματος θα ήθελα να λαμβάνω τις αιτήσεις για δημιουργία νέων καταστημάτων μέσω e-mail.

--- MyNightOut/src/mynightout/presenters/Presenter.java
+++ MyNightOut/src/mynightout/presenters/Presenter.java
@@ -6,31 +6,15 @@
 
 package mynightout.presenters;
 
+import mynightout.controllers.EmailSenderController;
 import mynightout.ui.IContactAdminForm;
-import mynightout.ui.IStoreRegisterForm;
+import mynightout.ui.StoreRegisterForm;
 
 /**
  *
  * @author Mustaine
  */
 public class Presenter implements IPresenter{
-    @Override
-    public void storeregister(IStoreRegisterForm form, String DesiredUsername, String DesiredPassword,
-            String CompanyName, String Street, String Postcode, String Telephone1, String Telephone2,
-            String Mobile, String Fax, String Email){
-        
-        if(DesiredUsername.equalsIgnoreCase(DesiredPassword)){
-            //TO DO: Αποστολή στοιχείων με e-mail.
-            
-            form.display("Επιτυχής Αποστολή!");
-            
-        }
-        else{
-            form.display("Αποτυχία Αποστολής!");
-        }
-        
-        
-    }
     
     
      @Override
@@ -39,4 +23,31 @@ public class Presenter implements IPresenter{
         
     }
     
-}
+    @Override
+    public void sendEmail(StoreRegisterForm form, String DesiredUsername, String DesiredPassword,
+            String CompanyName, String Street, String Postcode, String Telephone1, String Telephone2,
+            String Mobile, String Fax, String Email){
+        String userMessage="Επιθυμητό Όνομα Χρήστη: "+DesiredUsername+"\n"+"Επιθυμητός Kωδικός Xρήστη: "+DesiredPassword+"\n"+"Επωνυμία Εταιρείας: "+CompanyName+"\n"
+                +"Διεύθυνση Εταιρείας: "+Street+"\n"+"Ταχυδρομικός Κώδικας: "+Postcode+"\n"+"Τηλέφωνο 1: "+Telephone1+"\n"+"Τηλέφωνο 2: "+Telephone2+"\n"
+                +"Mobile: "+Mobile+"\n"+"fax: "+Fax+"\n"+"Email: "+Email;
+        
+        String to = "mynightoutdevelopers@gmail.com";
+        
+        if(EmailSenderController.sendMail("mynightouttest@gmail.com", "dge457hdw3", userMessage, to)){
+            System.out.println("Επιτυχής Αποστολή!");
+            form.display("Επιτυχής Αποστολή!");
+        }
+        else{
+            System.out.println("Παρουσιάστηκε κάποιο πρόβλημα.");
+            form.display("Παρουσιάστηκε κάποιο πρόβλημα, ξαναπροσπαθήστε.");
+        }
+        
+            
+        }
+
+   
+        
+    }
+    
+    
+
