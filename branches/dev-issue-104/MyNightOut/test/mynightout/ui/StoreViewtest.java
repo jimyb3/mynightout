44249380c00a2457f1dbfc5d8578f1e505a56514
punisherakias2@hotmail.com <punisherakias2@hotmail.com@66860919-3fcc-04c9-45bb-785dd833bd12>
2013-12-12 01:40:02 +0000

    Πληροτητα Καταστήματος (DB)

---
+++ branches/dev-issue-104/MyNightOut/test/mynightout/ui/StoreViewtest.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mynightout.ui;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.uispec4j.UISpec4J;
+import org.uispec4j.UISpecTestCase;
+import org.uispec4j.Window;
+import org.uispec4j.interception.MainClassAdapter;
+
+/**
+ *
+ * @author Mustaine
+ */
+public class StoreViewtest extends UISpecTestCase {
+    static {
+      UISpec4J.init();
+    }
+    
+    public StoreViewtest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        setAdapter(new MainClassAdapter(StoreView.class, new String[0]));
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+   //-----------------------------GUI Testing---------------------------------------//
+    
+    //Έλεγχος εμφάνισης φόρμας ή αν η φόρμα εμποδίζεται δυναμικά από κάποια 
+    //κλάση και εσφαλμένως δεν εμφανίζεται γενικά.
+    @Test
+    public void testvisibility() {
+          
+        Window window = getMainWindow();
+        
+        assertTrue(window.isVisible());         
+        
+        
+    }
+    
+    
+    
+    //Έλεγχος εμφάνισης των διάφορων JLabels της φόρμας.
+    @Test
+    public void testalllabels() {
+          
+        Window window = getMainWindow();
+        
+        assertTrue(window.containsLabel("Πληροφορίες Καταστήματος")); 
+               
+       
+   }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
