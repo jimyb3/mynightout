daf060cbd08814b8cf642a5adc09b1c29df8d184
ioanna65@hotmail.com <ioanna65@hotmail.com@66860919-3fcc-04c9-45bb-785dd833bd12>
2013-12-12 21:44:01 +0000

    commit-έτοιμη η φόρμα προβολής και επεξεργασίας προσωπικών στοιχείων του χρήστη και ο controller της,  μένει να γραφού μερικά τεστ και μετά merge to trunk

---
+++ branches/dev-issue-15/MyNightOut/MyNightOut/src/mynightout/controllers/EditUserInformationController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mynightout.controllers;
+
+import mynightout.dao.UserDao;
+import mynightout.entity.User;
+
+/**
+ *
+ * @author ioanna
+ */
+public class EditUserInformationController {
+
+    //private String currentUserName;
+    //  public EditUserInformationController(String cUserName) {
+    //    currentUserName = cUserName;
+    //}
+    public User getUserInfo(String currentUserName) {
+        try {
+            UserDao currentUser = new UserDao();
+            User user = currentUser.getUserDataByUsername(currentUserName);
+            return user;
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Πρόβλημα στην βάση");
+        }
+    }
+
+    public User saveChanges(String userName, String password, String customerName,
+            String customerLastname, String telephoneNum, String email) {
+        try {
+            UserDao currentUser = new UserDao();
+            User user = currentUser.updateUserData(userName, password, customerName, customerLastname, telephoneNum, email);
+            return user;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Πρόβλημα στην βάση");
+        }
+
+    }
+}
---
+++ branches/dev-issue-15/MyNightOut/MyNightOut/test/mynightout/controllers/EditUserInformationControllerTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mynightout.controllers;
+
+import mynightout.entity.User;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ioanna
+ */
+public class EditUserInformationControllerTest {
+    
+    public EditUserInformationControllerTest() {
+    }
+
+    /**
+     * Test of getUserInfo method, of class EditUserInformationController.
+     */
+    @Test
+    public void testGetUserInfoSuccess() {
+        System.out.println("getUserInfo");
+        String currentUserName = "user1";
+        EditUserInformationController instance = new EditUserInformationController();
+        //User expResult = ;
+        User result = instance.getUserInfo(currentUserName);
+        //assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of saveChanges method, of class EditUserInformationController.
+     */
+    @Test
+    public void testSaveChanges() {
+        System.out.println("saveChanges");
+        String userName = "";
+        String password = "";
+        String customerName = "";
+        String customerLastname = "";
+        String telephoneNum = "";
+        String email = "";
+        EditUserInformationController instance = new EditUserInformationController();
+        User expResult = null;
+        User result = instance.saveChanges(userName, password, customerName, customerLastname, telephoneNum, email);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
